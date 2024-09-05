b549a17fef76d36d1fa8e9b32e8d4921535a91b9
hareeen <me@hareen.io>
2024-09-06 01:34:27 +0900

    fix: resolve overflow issue

--- src/components/layout/Post.tsx
+++ src/components/layout/Post.tsx
@@ -26,7 +26,7 @@ export async function Post(props: { slug: string[] }) {
 
   return (
     <ProseArticle className="[&>*:nth-child(4)]:mt-0">
-      <p className="text-muted-foreground text-sm mb-0 prose-a:no-underline prose-a:text-muted-foreground hover:prose-a:underline">
+      <p className="text-muted-foreground md:text-sm text-xs mb-0 max-md:mb-0 prose-a:no-underline prose-a:text-muted-foreground hover:prose-a:underline">
         {subpaths.map((path) => (
           <Fragment key={`path-link-${path}`}>
             <Link href={`/b/${path.join('/')}`}>{path.length === 0 ? '~' : path[path.length - 1]}</Link>
@@ -34,8 +34,8 @@ export async function Post(props: { slug: string[] }) {
           </Fragment>
         ))}
       </p>
-      <h1 className="mt-2 mb-0">{frontmatter.title}</h1>
-      <p className="text-muted-foreground text-sm mt-2 mb-6 font-medium print:hidden">
+      <h1 className="mt-2 max-md:mt-1.5 mb-0 max-md:mb-0">{frontmatter.title}</h1>
+      <p className="text-muted-foreground md:text-sm text-xs mt-2 max-md:mt-1.5 mb-6 max-md:mb-5 font-medium print:hidden">
         {DateTime.fromJSDate(frontmatter.date)
           .setLocale((header.get('Accept-Language')?.split(',') ?? ['ko-KR'])[0])
           .toLocaleString(DateTime.DATE_MED)}
--- src/components/layout/ProseArticle.tsx
+++ src/components/layout/ProseArticle.tsx
@@ -7,7 +7,10 @@ export const ProseArticle = React.forwardRef<
 >((props, ref) => {
   return (
     <article
-      className={cn(props.className, 'prose max-md:prose-sm prose-zinc dark:prose-invert my-8 min-w-full')}
+      className={cn(
+        props.className,
+        'prose max-md:prose-sm prose-zinc dark:prose-invert my-8 min-w-full overflow-clip',
+      )}
       ref={ref}
     >
       {props.children}
